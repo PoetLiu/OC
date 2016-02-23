d7a55bb19611826fbe8b694e6a699aea2a299617
liupeng <liupeng@liupengs-mac.local>
2016-02-23 17:31:02 +0800

    Hello, world for OC

---
+++ Hello Objective-C/Hello Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by liupeng on 2/23/16.
+//  Copyright © 2016 liupeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
