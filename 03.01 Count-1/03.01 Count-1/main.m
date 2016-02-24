56dbbcbc62299e6582ee202569390544243e18a0
liupeng <liupeng@liupengs-mac.local>
2016-02-24 10:38:06 +0800

    a program to output number 1-5

---
+++ 03.01 Count-1/03.01 Count-1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  03.01 Count-1
+//
+//  Created by liupeng on 16/2/24.
+//  Copyright © 2016年 liupeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    NSLog(@"THe number from 1 to 5:");
+		for (int i = 1; i <= 5; i++) {
+			NSLog(@"%d", i);
+		}
+	}
+    return 0;
+}
