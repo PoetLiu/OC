288f17a5340a2908a7e553cb3ca119a0994f13c5
liuPeng <liupeng@192.168.2.5>
2016-03-01 21:36:59 +0800

    Change file path.

---
+++ 06.01 CarParts-Split/06.01 CarParts-Split/Tire.h
@@ -0,0 +1,12 @@
+//
+//  Tire.h
+//  06.01 CarParts-Split
+//
+//  Created by liupeng on 16/2/26.
+//  Copyright © 2016年 liupeng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Tire : NSObject
+@end // Tire
