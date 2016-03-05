9d0e1f669808ba67d7dff789ca31292a6b738622
liuPeng <liupeng@liupengdeimac.local>
2016-03-05 10:59:16 +0800

    file change.

--- 15.01 CaseTool/15.01 CaseTool/AppDelegate.m
+++ 15.01 CaseTool/15.01 CaseTool/AppDelegate.m
@@ -2,44 +2,49 @@
 //  AppDelegate.m
 //  15.01 CaseTool
 //
-//  Created by liupeng on 16/3/4.
-//  Copyright © 2016年 liupeng. All rights reserved.
+//  Created by liuPeng on 16/3/5.
+//  Copyright © 2016年 liuPeng. All rights reserved.
 //
 
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
 
+@property (weak) IBOutlet NSWindow *window;
 @end
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	// Override point for customization after application launch.
-	return YES;
+- (id) init
+{
+    if (nil != (self = [super init])) {
+        NSLog(@"init:text %@ / results %@", _textField, _resultsField);
+    }
+    return (self);
+}
+- (void) awakeFromNib
+{
+    NSLog(@"awake: text %@ / results %@", _textField, _resultsField);
+    [_textField setStringValue:@"Enter text here"];
+    [_resultsField setStringValue:@"Results"];
+}
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (IBAction)uppercae:(id)sender {
+    NSString *original  = [_textField stringValue];
+    NSString *uppercase = [original uppercaseString];
+    [_resultsField setStringValue:uppercase];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+- (IBAction)lowercase:(id)sender {
+    NSString *orignal = [_textField stringValue];
+    NSString *lowercase = [orignal lowercaseString];
+    [_resultsField setStringValue:lowercase];
 }
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 @end
