24daa0cec6efe71d1be5e7bf690e4f87cae45cd0
liupeng <liupeng@liupengs-mac.local>
2016-03-14 17:32:39 +0800

    Add support for data save to disk.

--- learn/RoadMapiOS/ToDoList/ToDoList/ToDoItem.h
+++ learn/RoadMapiOS/ToDoList/ToDoList/ToDoItem.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface ToDoItem : NSObject
+@interface ToDoItem : NSObject <NSCoding>
 @property NSString *itemName;
 @property BOOL completed;
 @property (readonly) NSDate *creationDate;
