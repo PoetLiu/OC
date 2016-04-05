bf65df9f9395c930b66b047ef51345828130a761
liupeng <liupeng@liupengs-mac.local>
2016-04-05 18:39:45 +0800

    A demo for web service.

---
+++ learn/iOSProgramming/Photorama/Photorama/PhotosViewController.m
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.m
+//  Photorama
+//
+//  Created by liupeng on 16/4/5.
+//  Copyright © 2016年 liupeng. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@implementation PhotosViewController
+/*
+ https://69.147.76.173/services/rest/?method=flickr.photos.getRecent&api_key=a6d819499131071f158fd740860a5a88&extras=url_h,date_taken&format=json&nojsoncallback=1
+ */
+@end
