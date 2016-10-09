059a77fc5e192ec49144a026408d7bcbbc99dfe3
liupeng <liupeng@liupengs-mac.local>
2016-10-09 14:28:09 +0800

    Add AFNetworking Test.

---
+++ Network/AFNetworkingTest/AFNetworkingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingTest
+//
+//  Created by liupeng on 16/10/9.
+//  Copyright © 2016年 liupeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Network/AFNetworkingTest/AFNetworkingTest/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  AFNetworkingTest
+//
+//  Created by liupeng on 16/10/9.
+//  Copyright © 2016年 liupeng. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	NSString *urlString = @"http://192.168.0.1/webnoauth/router_function_list.cgi";
+	NSURL *url = [NSURL URLWithString:urlString];
+	AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+	[manager GET:url.absoluteString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+		NSLog(@"results: %@", responseObject);
+	} failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+		NSLog(@"results: %@", error);
+	}];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
