8ed521244f8eb67e78c5d8539ddc380a197743c1
liupeng <liupeng@liupengs-mac.local>
2016-12-21 18:28:42 +0800

    Add del button

--- MyApps/Calculator/Calculator/ViewController.m
+++ MyApps/Calculator/Calculator/ViewController.m
@@ -18,7 +18,7 @@ typedef NS_ENUM(NSUInteger, CalcStateType) {
 };
 
 @interface ViewController ()
-@property (nonatomic, strong) UITextField *resultField;
+@property (nonatomic, strong) UITextView *resultView;
 @property (nonatomic, strong) NSMutableArray *buttons;
 @property (nonatomic, strong) NSArray *buttonLables;
 @property (nonatomic, assign) CGFloat screenHeight;
@@ -31,8 +31,10 @@ typedef NS_ENUM(NSUInteger, CalcStateType) {
 
 @implementation ViewController
 
-static const NSInteger BUTTON_SPACE_VERTICAL = 1.0f;
-static const NSInteger BUTTON_SPACE_HORIZONTAL = 1.0f;
+static const NSInteger BUTTON_SPACE_VERTICAL	= 1;
+static const NSInteger BUTTON_SPACE_HORIZONTAL	= 1;
+static const NSInteger BUTTON_LINES				= 7;
+static const NSInteger BUTTON_COLS				= 5;
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
@@ -40,45 +42,52 @@ static const NSInteger BUTTON_SPACE_HORIZONTAL = 1.0f;
 	_calculator		= [[Calculator alloc] init];
 	_screenHeight	= [UIScreen mainScreen].bounds.size.height;
 	_screenWidth	= [UIScreen mainScreen].bounds.size.width;
-	_buttonHeight	= _screenHeight/7.0;
-	_buttonWidth	= _screenWidth/4.0;
+	_buttonHeight	= _screenHeight/BUTTON_LINES;
+	_buttonWidth	= _screenWidth/BUTTON_COLS;
 	_state			= CalcStateNormal;
-	self.resultField	= [[UITextField alloc] init];
-	_resultField.placeholder	= @"0";
-	_resultField.userInteractionEnabled	= NO;
-	_resultField.textAlignment	= NSTextAlignmentRight;
-	_resultField.font	= [UIFont systemFontOfSize:80];
-	_resultField.contentVerticalAlignment	= UIControlContentVerticalAlignmentBottom;
-	_resultField.contentHorizontalAlignment	= UIControlContentHorizontalAlignmentRight;
-	_resultField.adjustsFontSizeToFitWidth = YES;
-	_resultField.translatesAutoresizingMaskIntoConstraints	= NO;
-	[self.view addSubview:_resultField];
-	[_resultField.topAnchor constraintEqualToAnchor:self.view.topAnchor].active	= YES;
-	[_resultField.widthAnchor constraintEqualToAnchor:self.view.widthAnchor].active	= YES;
-	[_resultField.heightAnchor constraintEqualToConstant:_screenHeight/7*2].active	= YES;
+	self.resultView	= [[UITextView alloc] init];
+	_resultView.scrollEnabled	= YES;
+	_resultView.editable	= NO;
+	_resultView.userInteractionEnabled	= YES;
+	_resultView.showsHorizontalScrollIndicator	= YES;
+	_resultView.showsVerticalScrollIndicator	= YES;
+	_resultView.textAlignment	= NSTextAlignmentRight;
+	_resultView.font	= [UIFont systemFontOfSize:80];
+	_resultView.translatesAutoresizingMaskIntoConstraints	= NO;
+	[self.view addSubview:_resultView];
+	[_resultView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active	= YES;
+	[_resultView.widthAnchor constraintEqualToAnchor:self.view.widthAnchor].active	= YES;
+	[_resultView.heightAnchor constraintEqualToConstant:_screenHeight/7*2].active	= YES;
 	
-    self.buttonLables	= @[@"7", @"8", @"9", @"+",
-							@"4", @"5", @"6", @"-",
-							@"1", @"2", @"3", @"*",
-							@"0", @"(",	@")", @"/",
-							@".", @"C", @"=", @"="];
+    self.buttonLables	= @[@"7", @"8", @"9", @"(", @")",
+							@"4", @"5", @"6", @"+", @"-",
+							@"1", @"2", @"3", @"*", @"/",
+							@"0", @".",	@"c", @"<-", @"<-",
+							@"=", @"=", @"=", @"=", @"="];
 	
 	for (NSInteger i = 0; i < self.buttonLables.count; i++) {
 		NSString *label	= [self.buttonLables objectAtIndex:i];
 		NSInteger width = 1, height = 1;
 		switch ([label characterAtIndex:0]) {
 			case '=':
-				width	= 2;
+				width	= 5;
 				break;
+			case '<':
+				width	= 2;
 			default:
 				break;
 		}
-        [self addButtonWithTitle:label row:i%4 line:i/4 width:width height:height];
+        [self addButtonWithTitle:label row:i%BUTTON_COLS line:i/BUTTON_COLS width:width height:height];
 	}
 }
 
 
-- (void)addButtonWithTitle:(NSString *)title row:(NSInteger)row line:(NSInteger)line width:(NSInteger)width height:(NSInteger)height {
+- (void)addButtonWithTitle:(NSString *)title
+					   row:(NSInteger)row
+					  line:(NSInteger)line
+					 width:(NSInteger)width
+					height:(NSInteger)height
+{
 	for (UIButton *b in _buttons) {
 		if ([title isEqualToString:[b titleForState:UIControlStateNormal]]) {
 			return;
@@ -98,8 +107,8 @@ static const NSInteger BUTTON_SPACE_HORIZONTAL = 1.0f;
 	[_buttons addObject:new];
 	[self.view addSubview:new];
 	new.translatesAutoresizingMaskIntoConstraints	= NO;
-	[new.topAnchor constraintEqualToAnchor:_resultField.bottomAnchor constant:line*_buttonHeight].active	= YES;
-	[new.leadingAnchor constraintEqualToAnchor:_resultField.leadingAnchor constant:row*_buttonWidth].active	= YES;
+	[new.topAnchor constraintEqualToAnchor:_resultView.bottomAnchor constant:line*_buttonHeight].active	= YES;
+	[new.leadingAnchor constraintEqualToAnchor:_resultView.leadingAnchor constant:row*_buttonWidth].active	= YES;
 	[new.heightAnchor constraintEqualToConstant:_buttonHeight*height-BUTTON_SPACE_VERTICAL].active	= YES;
 	[new.widthAnchor constraintEqualToConstant:_buttonWidth*width-BUTTON_SPACE_HORIZONTAL].active	= YES;
 }
@@ -113,7 +122,7 @@ static const NSInteger BUTTON_SPACE_HORIZONTAL = 1.0f;
 	NSString *result = nil;
     [self voiceBtnPressPlay:self];
 	if (self.state == CalcStateError) {
-		self.resultField.text	= nil;
+		self.resultView.text	= nil;
 		self.state	= CalcStateNormal;
 	}
 	switch ([[sender titleForState:UIControlStateNormal] characterAtIndex:0]) {
@@ -134,22 +143,27 @@ static const NSInteger BUTTON_SPACE_HORIZONTAL = 1.0f;
 		case '/':
 		case '(':
 		case ')':
-			result	= [self.resultField.text stringByAppendingString:[sender titleForState:UIControlStateNormal]];
+			result	= [self.resultView.text stringByAppendingString:[sender titleForState:UIControlStateNormal]];
 			break;
 		case '=':
-			result	= [self.calculator calculateWithExpression:self.resultField.text withError:&error];
+			result	= [self.calculator calculateWithExpression:self.resultView.text withError:&error];
 			break;
-		case 'C':
+		case 'c':
 			result	= nil;
+			break;
+		case '<':
+			if ([self.resultView.text length]) {
+				result	= [self.resultView.text substringToIndex:[self.resultView.text length]-1];
+			}
 			break;
 	}
 	
 	if (error) {
 		NSLog(@"Error:%@", error);
 		self.state				= CalcStateError;
-		self.resultField.text	= @"error";
+		self.resultView.text	= @"error";
 	} else {
-		self.resultField.text	= result;
+		self.resultView.text	= result;
 	}
 	return;
 }
